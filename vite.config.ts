88e6172acd0514dd962535bd720054c253da68df
Gaurav Sharma <gaurav.sharma@pocketfm.com>
2024-04-23 18:36:45 +0530

    Added resources

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://github.com/Borewit/music-metadata-browser/issues/836
   plugins: [react(), NgmiPolyfill()],
 });
